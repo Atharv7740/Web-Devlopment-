00329e9491795c1bd9bb104ecf8ef0c7f841be05
Atharv7740 <atharv7740@gmail.com>
2024-03-12 19:17:19 +0530

    again continue with js

--- Learning Javascript/script.js
+++ Learning Javascript/script.js
@@ -1,3 +1,6 @@
-console.log(10+20);
-console.log("hello world");
-console.log("Javascript is very easy and fast language")
+console.log('hello developers');
+console.log("helllooooo");
+let n=5;
+for(let i=0;i<n;i++){
+    console.log(i,"Hello Everyone");
+}
