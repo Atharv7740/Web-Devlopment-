04f11e4f0d3071316140247b0722aadde0e17a7a
Atharv7740 <atharv7740@gmail.com>
2024-03-14 16:37:19 +0530

    learning query selector

--- Learning Javascript/script.js
+++ Learning Javascript/script.js
@@ -1,6 +1,13 @@
-console.log('hello developers');
-console.log("helllooooo");
-let n=5;
-for(let i=0;i<n;i++){
-    console.log(i,"Hello Everyone");
-}
+// console.log('hello developers');
+// console.log("helllooooo");
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log(i,"Hello Everyone");
+// }
+// let a="atharv"
+// console.log(a);
+// const rno=45;
+// console.log(rno)
+console.log('Coming from the console.log');
+
+
